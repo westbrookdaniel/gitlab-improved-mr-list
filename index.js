75211b5dc4e292c3cc7743db82fe6e7b6474887e
westy12dan <westy12dan@gmail.com>
2023-05-25 14:29:28 +0800

    Add author profile icon

--- index.js
+++ index.js
@@ -2,6 +2,27 @@
 if (
   /^https:\/\/gitlab\.*.*\/merge_requests\/?(\?.*)?$/.test(window.location.href)
 ) {
+  function createUserIcon(id, user) {
+    // Check if it already exists
+    document.getElementById(id) && document.getElementById(id).remove()
+
+    const img = document.createElement('img')
+    img.id = id
+    img.width = '24'
+    img.classList.add('avatar', 'avatar-inline')
+    img.style.marginTop = '4px'
+    img.style.marginRight = '10px'
+    img.style.width = '24px'
+    img.style.height = '24px'
+    img.loading = 'lazy'
+
+    if (user) {
+      img.src = user.avatar_url
+    }
+
+    return img
+  }
+
   // Util for adding a badge to the merge request
   function createThreadsBadge(id, color, resolved, resolvable) {
     // Check if it already exists
@@ -22,20 +43,40 @@ if (
       span.innerText = `${resolved}/${resolvable}`
     }
 
-    li.prepend(span)
+    li.append(span)
     return li
   }
 
   // For each merge request (using index as id)
   document.querySelectorAll('.merge-request').forEach((element, id) => {
+    // Ids for elements
+    const uid = id + 'user'
+    const tid = id + 'threads'
+
+    // Adjust the side padding because we're gonna add an avatar
+    element.style.paddingLeft = '4px'
+    element.style.paddingRight = '4px'
+
     const anchor = element.querySelector('.merge-request-title-text a')
+    const author = element.querySelector('.author-link')
+    const authorUsername = author.href.split('/').pop()
     const metaList = element.querySelector('.issuable-meta ul')
 
-    // Create loading placeholder
-    const gray = '#e3e3e3'
-    metaList.append(createThreadsBadge(id, gray))
+    // Create loading placeholders
+    element.prepend(createUserIcon(uid))
+    metaList.append(createThreadsBadge(tid, '#e3e3e3'))
 
-    // Fetch it's discussions
+    // Add a profile picture for the author of the MR
+    fetch(`/api/v4/users?username=${authorUsername}`)
+      .then((res) => res.json())
+      .then((res) => {
+        const user = res[0]
+        if (!user) return
+
+        element.prepend(createUserIcon(uid, user))
+      })
+
+    // Add a threads resolved / threads unresolved indicator
     fetch(`${anchor.href}/discussions.json`)
       .then((res) => res.json())
       .then((res) => {
@@ -51,15 +92,15 @@ if (
         if (resolvable > resolved) {
           // Not resolved
           const red = '#dd2b0e'
-          metaList.append(createThreadsBadge(id, red, resolved, resolvable))
+          metaList.append(createThreadsBadge(tid, red, resolved, resolvable))
         } else if (resolved === resolvable && resolvable > 0) {
           // All Resolved
           const green = '#108548'
-          metaList.append(createThreadsBadge(id, green, resolved, resolvable))
+          metaList.append(createThreadsBadge(tid, green, resolved, resolvable))
         } else {
           // No threads
           const gray = '#737278'
-          metaList.append(createThreadsBadge(id, gray, resolved, resolvable))
+          metaList.append(createThreadsBadge(tid, gray, resolved, resolvable))
         }
       })
   })
